f22d245364d806447ad30a870290f7ebf69a62ce
Madhusakth <madhumithasakthi.iyer@utexas.edu>
2022-03-08 03:49:02 +0000

    baseline code

--- vehicle_detection/src_cs/compressive_sensing_radar_radiate_polar.m
+++ vehicle_detection/src_cs/compressive_sensing_radar_radiate_polar.m
@@ -1,128 +1,85 @@
 %clear, close all, clc;
-function compressive_sensing_radar_v1(scene,meas) 
-%myDir = char(strcat('/home/ms75986/Desktop/Qualcomm/Radar-Samp/Adaptive-Radar-Acquisition/data/oxford-v1/scene', string(scene),'/radar'));
-%saveDir = char(strcat('/home/ms75986/Desktop/Qualcomm/Radar-Samp/Adaptive-Radar-Acquisition/data/oxford-v1/scene', string(scene),'/radar-recons-sparse-40-', meas));
+function compressive_sensing_radar_radiate_polar() 
 
 scenes={'city_3_7','night_1_4', 'motorway_2_2','snow_1_0','tiny_foggy'}
+
+
+
+samp_rate = 0.55;
 
 for scene_num=1:length(scenes)
 	scene = scenes{scene_num}
 
-myDir= char(strcat('/home/ms75986/Desktop/Qualcomm/RADIATE/radiate_sdk/data/radiate/',scene,'/Navtech_Polar'))
-saveDir=char(strcat('/home/ms75986/Desktop/Qualcomm/RADIATE/radiate_sdk/data/radiate/',scene,'/reconstruct-polar-same-meas-55'))
+	myDir= char(strcat('../../data/radiate/',scene,'/Navtech_Polar'))
+	saveDir=char(strcat('../../data/radiate/',scene,'/reconstruct-polar-same-meas-55'))
 
 
-myFiles = dir(fullfile(myDir,'*.png'));
-meas
-saveDir
-parallel = 19; length(myFiles);%12;
-outer_loop = [2:parallel:19];%length(myFiles)];
+	myFiles = dir(fullfile(myDir,'*.png'));
+	parallel = length(myFiles);
 
-if ~exist(saveDir, 'dir')
-       mkdir(saveDir)
-    end
-
-for p = 1:length(outer_loop)
-recons_full = [];
-snrs_full = [];
-MAE_full = [];
-saveFileNameFull = [];
-%if p == length(outer_loop)
-%    parallel = 2;
-%end
-%parallel = 1;
+	if ~exist(saveDir, 'dir')
+	       mkdir(saveDir)
+	    end
 
 
-for k = 1:parallel %%%%%%%%
-    current = outer_loop(p) + (k-1)
-    baseFileName = myFiles(current).name;
-    fullFileName = fullfile(myDir, baseFileName);
-    disp(fullFileName)
-    A = imread(fullFileName);
-    %baseFileName = strrep(baseFileName, '.png', '.mat');
-    %meta = A(:, 1:11); 
-    %A = A(:,12:3779);
+	for current = 1:parallel
+	    baseFileName = myFiles(current).name;
+	    fullFileName = fullfile(myDir, baseFileName);
+	    disp(fullFileName)
+	    A = imread(fullFileName);
 
-    A = A.';
-    [height, width] = size(A);
-    %A = A([1:50],[12:87]);
-    w = 20;
-    h = 48; %112 - 12; %87 - 12;
-    rate = double(int16((w*h)*0.55));
+	    A = A.';
+	    [height, width] = size(A);
+	    w = 20;
+	    h = 48; 
+	    rate = double(int16((w*h)*samp_rate));
 
+	    I = eye(w*h);
+	    I = I(1:floor(samp_rate*w*h),1:w*h);
+	    cols = size(I,2);
+	    P = randperm(cols);
+	    Phi = I(:,P);
 
-    %Phi = randn(floor(0.1*50*50),50*50);
-
-    %%%%
-    I = eye(w*h);
-    I = I(1:floor(0.55*w*h),1:w*h);
-    cols = size(I,2);
-    P = randperm(cols);
-    Phi = I(:,P);
-    %%%%
-
-
-    snrs = [];
-    MAEs = [];
-    rows = [1: w: 401]
-    columns = [1: h: 577]
-    final_A = [];
-    final_rate = 0;
-    for c = 1:length(rows)-1
-        c,rate;
-        final_A_column = [];
-        for d = 1:length(columns)-1
-	    final_rate = final_rate + rate;
-            %final_rate, rate
-            A_ = A([rows(c):rows(c+1)-1],[columns(d):columns(d+1)-1]);
-	    %continue %%%%%%%%%%
-            %x1 = compressed_sensing_example(A_, w, h, rate, meas);
-	    x1 = compressed_sensing_example_parallel(A_, w, h, rate,Phi); %%%%
-            x1 = uint8(x1);
-            peak = psnr(A_,x1);
-            snrs = [snrs;peak];
-            MAE=sum(abs(A_(:)-x1(:)))/numel(A_);
-            MAEs = [MAEs; MAE];
+	    snrs = [];
+	    MAEs = [];
+	    rows = [1: w: 401];
+	    columns = [1: h: 577];
+	    final_A = [];
+	    final_rate = 0;
+	    for c = 1:length(rows)-1
+		c,rate;
+		final_A_column = [];
+		for d = 1:length(columns)-1
+		    final_rate = final_rate + rate;
+		    A_ = A([rows(c):rows(c+1)-1],[columns(d):columns(d+1)-1]);
+		    x1 = compressed_sensing_example_parallel(A_, w, h, rate,Phi); %%%%
+		    x1 = uint8(x1);
+		    peak = psnr(A_,x1);
+		    snrs = [snrs;peak];
+		    MAE=sum(abs(A_(:)-x1(:)))/numel(A_);
+		    MAEs = [MAEs; MAE];
             final_A_column = horzcat(final_A_column, x1);
-            
-        end
-        final_A = vertcat(final_A, final_A_column);        
-    end
-    final_rate
-    %continue %%%%%%%%%%
-    %final_A_meta = final_A; %horzcat(meta, final_A);
-    %recons_full = [recons_full ; final_A_meta];
-    %snrs_full = [snrs_full; snrs];
-    %MAE_full = [MAE_full; MAEs];
-    final_A = final_A.';
-    [recons_h, recons_w] = size(final_A);
-    final_A_reshaped = zeros(576,400);
-    recons_h
-    recons_w
-    final_A_reshaped(1:recons_h,1:recons_w) = final_A;
-    final_A_reshaped = uint8(final_A_reshaped);
-    fullFileNameRecons = fullfile(saveDir, baseFileName);
-    imwrite(final_A_reshaped,fullFileNameRecons);
-    baseFileName = strrep(baseFileName, '.png', '.mat');
-    fullFileNameRecons = fullfile(saveDir, baseFileName);
-    save(fullFileNameRecons, 'snrs', 'MAEs')
-    mean(snrs)
-    mean(MAEs)
+		    
+		end
+		final_A = vertcat(final_A, final_A_column);        
+	    end
+	    final_rate
+	    final_A = final_A.';
+	    [recons_h, recons_w] = size(final_A);
+	    final_A_reshaped = zeros(576,400);
+	    recons_h
+	    recons_w
+	    final_A_reshaped(1:recons_h,1:recons_w) = final_A;
+	    final_A_reshaped = uint8(final_A_reshaped);
+	    fullFileNameRecons = fullfile(saveDir, baseFileName);
+	    imwrite(final_A_reshaped,fullFileNameRecons);
+	    baseFileName = strrep(baseFileName, '.png', '.mat');
+	    fullFileNameRecons = fullfile(saveDir, baseFileName);
+	    save(fullFileNameRecons, 'snrs', 'MAEs')
+	    mean(snrs)
+	    mean(MAEs)
 
-    %saveFileNameFull = [saveFileNameFull; fullFileNameRecons];
-end
-for n = 1:parallel
-continue
-final_A_meta = recons_full((n-1)*1152 + 1: n*1152, :);
-snrs = snrs_full((n-1)*529 +1 : n*529);
-MAEs = MAE_full((n-1)*529 +1 : n*529);
-fullFileNameRecons = saveFileNameFull(n,:);
-imwrite(final_A,fullFileNameRecons);
-mean(snrs)
-mean(MAEs)
-%save(fullFileNameRecons, 'final_A_meta', 'snrs', 'MAEs')
-end
-end
+	end
 end
 
 end
--- vehicle_detection/src_cs/main_script_polar_all_final_10.sh
+++ vehicle_detection/src_cs/main_script_polar_all_final_10.sh
@@ -1,9 +1,5 @@
 echo "-----------started processing first image-----------"
 
-#radarDir='/home/ms75986/Desktop/Qualcomm/Radar-Samp/Adaptive-Radar-Acquisition/data/oxford-v1/scene3/final-img-rad-info/'
-#saveDir='/home/ms75986/Desktop/Qualcomm/Radar-Samp/Adaptive-Radar-Acquisition/data/oxford-v1/scene3/radar-pcd-data/'
-
-
 scenes='city_3_7 night_1_4 motorway_2_2 snow_1_0 tiny_foggy'
 
 
@@ -24,7 +20,7 @@ do
    cd /home/ms75986/Desktop/Qualcomm/RADIATE/radiate_sdk/vehicle_detection/src_cs
    python3 script_peak_detect_polar_final.py --npy_name=$prev --scene=$scene --sr=$sr
    echo "Processing $i th image $scene"
-   matlab -nodesktop -nosplash -c 3221@harrison.ece.utexas.edu -r "var1='$scene';var2='$folder'; run compressed_sensing_radar_pcd_bash_polar_final_1($i,var1,var2,$sr)"
+   matlab -nodesktop -nosplash -c 3221@harrison.ece.utexas.edu -r "var1='$scene';var2='$folder'; run compressed_sensing_radar_pcd_bash_polar($i,var1,var2,$sr)"
    fi
 done
 done
